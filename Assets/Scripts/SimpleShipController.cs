904ac829a559235dfa4d3d97f4881c4db8efb017
Nicolas Gerard <dhai@Nicolass-MacBook-Pro.local>
2018-04-23 13:38:00 +0900

    controll basics

---
+++ Assets/Scripts/SimpleShipController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleShipController : MonoBehaviour 
+{	
+	void Update ()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		transform.Translate(0, y, 0);
+		transform.Rotate(0, 0, -x);
+	}
+}
---
+++ Assets/Scripts/TransformLooper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Objectif :
+ * Faire boucler la position de l'objet dans une zone rectangulaire.
+ * Lorsque l'objet sors sur le bord droit, il revient sur à gauche.
+ */
+
+[AddComponentMenu ("SpaceShooter/Transform Looper")]
+public class TransformLooper : MonoBehaviour {
+
+	public Rect area = new Rect(0,0,10, 10);
+
+	void Update()
+	{
+		Vector3 position = transform.position;
+
+		if (area.Contains(position))
+			return;
+
+		if (position.x < area.xMin)
+			position.x = area.xMax;
+		else if (position.x > area.xMax)
+			position.x = area.xMin;
+
+		if (position.y < area.yMin)
+			position.y = area.yMax;
+		else if (position.y > area.yMax)
+			position.y = area.yMin;
+
+		transform.position = position;
+	}
+}
